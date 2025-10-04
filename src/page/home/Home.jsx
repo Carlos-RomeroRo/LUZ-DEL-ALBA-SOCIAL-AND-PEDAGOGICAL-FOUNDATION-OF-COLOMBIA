778cbd8b395da3109423a429fbf351055991dc48
Carlos Romero <romero.ca20042018@gmail.com>
2025-10-04 14:58:29 -0500

    feat: add animations in home and fix cards

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,14 +1,32 @@
-import '../../components/navbar/navBar'
 import { Card } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import AnimatedContent from "../../components/animations/animateComponents/animatedContent/animatedContent";
+import FadeContent from '../../components/animations/animateComponents/fadeContent/fadeContent';
+import Particles from '../../components/animations/background/particles/particles';
+import BlurText from '../../components/animations/text/blurText/blurText';
+import TextType from '../../components/animations/text/textType/textType';
+import '../../components/navbar/navBar';
 
 
 export default function Home() {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Solo se ejecuta una vez
+    threshold: 0.3, // Se activa cuando el 30% del elemento es visible
+  });
+  const [showButton, setShowButton] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => setShowButton(true), 4800);
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
-    <div className="relative w-full min-h-screen mx-auto ">
+    <div className="relative w-full min-h-screen mx-auto overflow-hidden">
       <section className="relative flex items-center justify-center min-h-screen w-full">
         {/* Video de fondo */}
         <video
-          src="https://videocdn.cdnpk.net/videos/3de5ee2b-160f-41ae-bfb5-8383895e6d15/horizontal/previews/clear/large.mp4?token=exp=1759335028~hmac=eae8d46272de77f5eca40fe3dde2d7edf5a2adde1c3d780e798fa958ba0d23a2"
+          src="https://videocdn.cdnpk.net/videos/3de5ee2b-160f-41ae-bfb5-8383895e6d15/horizontal/previews/clear/large.mp4?token=exp=1759590261~hmac=d1a92020795dbde0bd32184c8ab41ebd0412315ad499bd40885d78a916148c41"
           autoPlay
           muted
           loop
@@ -16,7 +34,7 @@ export default function Home() {
           className="absolute inset-0 w-full h-full object-cover -z-10"
         ></video>
 
-        {/* Capa oscura */}
+        {/* Capa oscura */} 
         <div className="absolute inset-0 bg-black bg-opacity-60"></div>
 
         {/* Contenido */}
@@ -26,144 +44,200 @@ export default function Home() {
             <span className="text-yellow-400">LUZ DEL ALBA</span>
           </h1>
 
-          <p className="mt-4 text-white text-[clamp(14px,2vw,20px)] font-medium">
-            La Fundación Social y Pedagógica de Colombia Luz del Alba es una entidad sin ánimo de lucro 
-            que busca reconstruir el tejido social mediante convenios con instituciones educativas en El Copey, 
-            beneficiando a personas en estado de abandono y vulnerabilidad.
-          </p>
-
-          <button
-            type="button"
-            className="mt-6 text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
-          >
-            Donar
-          </button>
-              <div className=" flex flex-col items-center mt-10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  className="w-12 h-12 stroke-white animate-bounce"
-                  fill="none"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
+          <BlurText
+            text="La Fundación Social y Pedagógica de Colombia Luz del Alba es una entidad sin ánimo de lucro que busca reconstruir el tejido social mediante convenios con instituciones educativas en El Copey, beneficiando a personas en estado de abandono y vulnerabilidad."
+            className="mt-4 text-white text-[clamp(14px,2vw,20px)] font-medium leading-relaxed max-w-3xl justify-center aling-middle"
+            onAnimationEnd={() => setShowButton(true)}
+          />
+          {showButton && (
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+              <div>
+                <button
+                  type="button"
+                  className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105 mt-4"
                 >
-                  <path d="M12 5v14" />
-                  <path d="m19 12-7 7-7-7" />
-              </svg>
-            </div>
+                  Donar
+                </button>
+              </div>
+            </FadeContent>
+          )}
+
+          <AnimatedContent
+            distance={150}
+            direction="horizontal"
+            reverse={false}
+            duration={5}
+            animateOpacity
+            scale={1.1}
+            threshold={0.2}
+            delay={5}
+          >
+              <div className=" flex flex-col items-center mt-10">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="w-12 h-12 stroke-white animate-bounce"
+                      fill="none"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <path d="M12 5v14" />
+                      <path d="m19 12-7 7-7-7" />
+                  </svg>
+                </div>
+          </AnimatedContent>
         </div>
+
       </section>
-        <section className='w-full h-auto py-10 px-5 overflow-hidden'>
-          <h1 className='flex justify-center align-middle text-white text-[clamp(28px,6vw,60px)] font-bold'>NUESTROS SERVICIOS</h1>
+      
+        <section className='relative w-full h-auto py-10 px-5 overflow-hidden'> 
+          {/* Fondo animado */}
+          <div className="absolute inset-0 -z-10">
+            <Particles
+              particleColors={['#12314D']}
+              particleCount={800}
+              particleSpread={10}
+              speed={0.1}
+              particleBaseSize={150}
+              moveParticlesOnHover={true}
+              alphaParticles={false}
+              disableRotation={false}
+            />
+          </div>
+
+
+          <div ref={ref} className="w-full flex justify-center items-center">
+            {inView && (
+              <TextType
+                text={["Nuestros servicios","¿Qué ofrecemos?","Nuestras especialidades"]}
+                typingSpeed={95}
+                pauseDuration={1500}
+                showCursor={true}
+                cursorCharacter="|"
+                className="text-white text-[clamp(28px,6vw,60px)] font-bold text-center"
+              />
+            )}
+          </div>
+          
           <div className="flex flex-wrap justify-center pt-12 gap-8">
-            <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
-              <img
-                className="w-full h-full object-cover"
-                src="https://images.pexels.com/photos/1148998/pexels-photo-1148998.jpeg"
-                alt="Acompañamiento social"
-              />
-              <div className="p-4">
-                <h5 className="text-2xl font-bold text-white text-center">
-                  Acompañamiento social
-                </h5>
-              </div>
-              <div className='flex flex-grow justify-center align-middle mb-4'>
-              <button
-                type="button"
-                className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
-              >
-                 Ver más
-              </button>
-              </div>
-            </Card>
-
-            <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
-              <img
-                className="w-full h-full object-cover"
-                src="https://images.pexels.com/photos/1648387/pexels-photo-1648387.jpeg"
-                alt="Asesorías enfocadas a familias"
-              />
-              <div className="p-4">
-                <h5 className="text-2xl font-bold text-white text-center">
-                  Asesorías enfocadas a familias
-                </h5>
-              </div>
-              <div className='flex flex-grow  justify-center align-middle mb-4'>
-              <button
-                type="button"
-                className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
-              >
-                 Ver más
-              </button>
-              </div>
-            </Card>
-
-            <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
-              <img
-                className="w-full h-full object-cover"
-                src="https://images.pexels.com/photos/3992949/pexels-photo-3992949.jpeg"
-                alt="Seguimiento pedagógico"
-              />
-              <div className="p-4">
-                <h5 className="text-2xl font-bold text-white text-center">
-                  Seguimiento pedagógico
-                </h5>
-              </div>
-              <div className='flex flex-grow justify-center align-middle mb-4'>
-              <button
-                type="button"
-                className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
-              >
-                 Ver más
-              </button>
-              </div>
-            </Card>
-
-            <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
-              <img
-                className="w-full h-full object-cover"
-                src="https://images.pexels.com/photos/208216/pexels-photo-208216.jpeg"
-                alt="Jornada de fortalecimiento espiritual"
-              />
-              <div className="p-4">
-                <h5 className="text-2xl font-bold text-white text-center">
-                  Jornada de fortalecimiento espiritual
-                </h5>
-              </div>
-              <div className='flex flex-grow justify-center align-middle mb-4'>
-              <button
-                type="button"
-                className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
-              >
-                 Ver más
-              </button>
-              </div>
-            </Card>
-
-            <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden transition-transform transform hover:scale-105 cursor-pointer">
-              <img
-                className="w-full h-full object-cover"
-                src="https://images.pexels.com/photos/1449934/pexels-photo-1449934.jpeg"
-                alt="Actividades lúdico-recreativas"
-              />
-              <div className="p-4">
-                <h5 className="text-2xl font-bold text-white text-center">
-                  Actividades <br />lúdico-recreativas
-                </h5>
-              </div>
-              <div className='flex flex-grow justify-center align-middle mb-4'>
-              <button
-                type="button"
-                className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
-              >
-                 Ver más
-              </button>
-              </div>
-            </Card>
-
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
+                <img
+                  className="w-full h-full object-cover"
+                  src="https://images.pexels.com/photos/1148998/pexels-photo-1148998.jpeg"
+                  alt="Acompañamiento social"
+                />
+                <div className="p-4">
+                  <h5 className="text-2xl font-bold text-white text-center">
+                    Acompañamiento social
+                  </h5>
+                </div>
+                <div className='flex flex-grow justify-center align-middle mb-4'>
+                <button
+                  type="button"
+                  className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
+                >
+                  Ver más
+                </button>
+                </div>
+              </Card>
+            </FadeContent>
             
-              
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="https://images.pexels.com/photos/1648387/pexels-photo-1648387.jpeg"
+                    alt="Asesorías enfocadas a familias"
+                  />
+                  <div className="p-4">
+                    <h5 className="text-2xl font-bold text-white text-center">
+                      Asesorías enfocadas a familias
+                    </h5>
+                  </div>
+                  <div className='flex flex-grow  justify-center align-middle mb-4'>
+                  <button
+                    type="button"
+                    className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
+                  >
+                    Ver más
+                  </button>
+                  </div>
+                </Card>
+            </FadeContent>
+            
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                 <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="https://images.pexels.com/photos/3992949/pexels-photo-3992949.jpeg"
+                    alt="Seguimiento pedagógico"
+                  />
+                  <div className="p-4">
+                    <h5 className="text-2xl font-bold text-white text-center">
+                      Seguimiento pedagógico
+                    </h5>
+                  </div>
+                  <div className='flex flex-grow justify-center align-middle mb-4'>
+                  <button
+                    type="button"
+                    className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
+                  >
+                    Ver más
+                  </button>
+                  </div>
+                </Card>
+            </FadeContent>
+           
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden  transition-transform transform hover:scale-105 cursor-pointer">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="https://images.pexels.com/photos/208216/pexels-photo-208216.jpeg"
+                    alt="Jornada de fortalecimiento espiritual"
+                  />
+                  <div className="p-4">
+                    <h5 className="text-2xl font-bold text-white text-center">
+                      Fortalecimiento espiritual
+                    </h5>
+                  </div>
+                  <div className='flex flex-grow justify-center align-middle mb-4'>
+                  <button
+                    type="button"
+                    className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
+                  >
+                    Ver más
+                  </button>
+                  </div>
+                </Card>
+            </FadeContent>
+            
+            <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+                <Card className="max-w-sm bg-[#12314D] rounded-lg overflow-hidden transition-transform transform hover:scale-105 cursor-pointer">
+                  <img
+                    className="w-full h-full object-cover"
+                    src="https://images.pexels.com/photos/1449934/pexels-photo-1449934.jpeg"
+                    alt="Actividades lúdico-recreativas"
+                  />
+                  <div className="p-4">
+                    <h5 className="text-2xl font-bold text-white text-center">
+                      Actividades <br />lúdico-recreativas
+                    </h5>
+                  </div>
+                  <div className='flex flex-grow justify-center align-middle mb-4'>
+                  <button
+                    type="button"
+                    className="text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-400 font-semibold rounded-full text-lg px-8 py-3 transition-transform transform hover:scale-105"
+                  >
+                    Ver más
+                  </button>
+                  </div>
+                </Card>
+            </FadeContent>
+                
+            
+                
           </div>
         </section>
     </div>
